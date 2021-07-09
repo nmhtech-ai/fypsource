fadb566c32115c4f5324e9914713ea8a70abe961
nmhtech-ai <nmhtech.ai@gmail.com>
2021-07-09 13:42:50 +0800

    Completed Topic Model, Routes, Controller

---
+++ models/topicModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const topicSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, "[REQUIRED ERROR] Code is required!"]
+    },
+    name: [
+        {
+            language: {
+                type: String
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    grade: {
+        type: String,
+        enum: {
+            values: ['P1', 'P2', 'P3', 'P4', 'P5', 'P6'],
+            message: '[ENUM ERROR] Grade is invalid!'
+        },
+        required: [true, "[REQUIRED ERROR] Grade is required!"]
+    },
+    semester: {
+        type: String,
+        enum: {
+            values: ['A', 'B'],
+            message: '[ENUM ERROR] Semester is invalid!'
+        },
+        required: [true, "[REQUIRED ERROR] Semester is required!"]
+    },
+    subtopicId: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Subtopic'
+        }
+    ],
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+},
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+topicSchema.pre(/^find/, function (next) {
+    // this.populate({
+    //     path: 'subtopicId',
+    //     select: '-__v'
+    // })
+    next();
+});
+
+topicSchema.pre('save', async function (next) {
+    this.updatedAt = Date.now() - 1000;
+    next();
+})
+
+const Topic = mongoose.model('Topic', topicSchema);
+module.exports = Topic;
